f66b05f8cd0bc49dbb83e95211b122ac00716c16
Grawlan <izikiel@gmail.com>
2022-05-20 21:40:06 +0200

    added authmiddleware + getme function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,6 +66,17 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    get current user info
+// @route   /api/users/getUser
+// @access  admin/user
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+  }
+  res.status(200).json(user)
+})
+
 // generate jtw token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -76,4 +87,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
