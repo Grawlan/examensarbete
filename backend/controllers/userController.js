88de6b523e0ee9d662ea10ca2f35ec16a92c4463
Grawlan <izikiel@gmail.com>
2022-05-20 19:13:16 +0200

    added jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
@@ -36,6 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user._id,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -56,12 +58,20 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json({
       _id: user._id,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
     throw new Error('Invalid email or password')
   }
 })
+
+// generate jtw token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
