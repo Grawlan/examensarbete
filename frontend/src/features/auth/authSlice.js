568c41e3c8daa8672d9021c66c822b2140348819
Grawlan <izikiel@gmail.com>
2022-05-24 18:54:19 +0200

    added cases with extrareducers

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authService from './authService'
 
 const initialState = {
   user: null,
@@ -12,7 +13,18 @@ const initialState = {
 export const createUser = createAsyncThunk(
   'auth/create_user',
   async (user, thunkAPI) => {
-    console.log(user)
+    try {
+      return await authService.register(user)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
   }
 )
 
@@ -28,7 +40,23 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+  },
 })
 
 export default authSlice.reducer
